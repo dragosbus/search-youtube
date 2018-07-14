6c9b6038275a2e55fbbdc955c06c602a5be37b87
dragosbus <dragosbusuioc94@gmail.com>
2018-07-14 08:53:44 +0200

    created components files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+const API_KEY = 'AIzaSyAEGffpXZAuR9_FpYlWSo5f28JW6ZgJgSE';
+
+class App extends Component {
+    render() {
+        return (
+            <h1>hggf</h1>
+        );
+    }
+}
+
+export default App;
