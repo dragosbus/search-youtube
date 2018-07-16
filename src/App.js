6e2b792c49261edc167e5fce67cd164e94cdafcd
dragosbus <dragosbusuioc94@gmail.com>
2018-07-16 08:27:38 +0200

    when the form is submited get data from api and setstate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
+import YTSearch from 'youtube-api-search';
 
 import Search from './components/Search';
-
 
 const API_KEY = 'AIzaSyAEGffpXZAuR9_FpYlWSo5f28JW6ZgJgSE';
 
 class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            videos: []
+        }
         this.submitSearch = this.submitSearch.bind(this);
     }
 
     submitSearch(value) {
-        console.log(value);
+        YTSearch({key:API_KEY, term:'space'}, data=> {
+            this.setState(prevState=>{
+                return {
+                    videos: prevState.videos.concat(data)
+                }
+            },()=>console.log(this.state.videos));
+        });
     }
 
     render() {
